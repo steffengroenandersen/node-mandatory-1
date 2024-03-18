7defe0797bc4643df817ef7fd73aa9eadb162e49
steffengroenandersen <steffengroenandersen@gmail.com>
2024-03-18 21:08:04 +0100

    Add HTML and css structure, and header+footer setup

--- routers/pagesRouter.js
+++ routers/pagesRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { homepagePage } from "../util/readPages.js"
+import { homepagePage, nodePage } from "../util/readPages.js"
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send(homepagePage);
 });
+
+router.get("/node", (req, res) => {
+  res.send(nodePage);
+})
 
 export default router;
--- util/readPages.js
+++ util/readPages.js
@@ -2,3 +2,6 @@ import { readPage, renderPage } from "./templateEngine.js"
 
 const homepage = readPage("./public/pages/homepage/homepage.html");
 export const homepagePage = renderPage(homepage);
+
+const node = readPage("./public/pages/node/node.html");
+export const nodePage = renderPage(node); 
